455f4b8fc11feefa570c65ce53681691e87e2f77
Wye-Lustre <30680577+Lustrew@users.noreply.github.com>
2019-02-13 13:42:22 +0800

    Create test.c

---
+++ src/test.c
@@ -0,0 +1,11 @@
+// usage: ./test image_bin_filename
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+#include "cnn.h"
+int main(int argc, char* argv[])
+{
+	return 0;
+}
